72048e64fe8a1e4a092c1e7a04da4f0c75bc43ad
matteoLorenzini <matteo-lorenzini@outlook.it>
2021-04-11 10:39:22 +0200

    Added uploader on main app

--- app.py
+++ app.py
@@ -2,10 +2,17 @@ from flask import Flask
 from flask import render_template
 from datetime import datetime
 import re
+import os
 import person_parser
 from flask import request
+from flask import redirect, url_for, abort
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+
+#app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024
+app.config['UPLOAD_EXTENSIONS'] = ['.xml', '.rdf']
+app.config['UPLOAD_PATH'] = 'input/person/'
 
 # Replace the existing home function with the one below
 @app.route("/")
@@ -29,5 +36,16 @@ def dynamic_page():
 @app.route("/person/")
 def person():
     return render_template("person.html")
+
+@app.route('/person/', methods=['POST'])
+def upload_files():
+    uploaded_file = request.files['file']
+    filename = secure_filename(uploaded_file.filename)
+    if filename != '':
+        file_ext = os.path.splitext(filename)[1]
+        if file_ext not in app.config['UPLOAD_EXTENSIONS']:
+            abort(400)
+        uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
+    return redirect(url_for('index'))
 
 
